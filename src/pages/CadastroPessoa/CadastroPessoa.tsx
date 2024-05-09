c4f3726c316e28324291c2fa94470d618089bed9
Brunodw123br <brunosilvandradeDev@gmail.com>
2024-05-09 07:33:31 -0300

    comeco criacao cadastroPessoa

--- src/pages/CadastroPessoa/CadastroPessoa.tsx
+++ src/pages/CadastroPessoa/CadastroPessoa.tsx
@@ -1,18 +1,65 @@
+import { useState } from "react";
 import HeaderPessoa from "../../componentes/HeaderPessoa/HeaderPessoa"
 import "./cadastroPessoa.css"
 export default function CadastroPessoa(){
+    const [telefone,setTelefone] = useState("");
+    const [cep,setCep] = useState("");
+    const [senha,setSenha] = useState("");
+    const [senhaRepetir,setSenhaRepetir] = useState("");
     return(
         <>
             <div className="containercadastroPessoa">
             <HeaderPessoa/>
-            <div className="formulario">
+            <div className="formularioCadastroPessoa">
                 <form>
-                <input type="text" placeholder="Nome Completo"/>
-                <input type="text" placeholder="E-mail"/>
-                <input type="text" placeholder="Telefone Celular"/>
-                <input type="text" placeholder="Cep"/>
-                <input type="text" placeholder="Senha"/>
-                <input type="text" placeholder="Repita sua Senha"/>
+                <input type="text"  maxLength={145} placeholder="Nome Completo"/>
+                <input type="email" placeholder="E-mail"/>
+                <input type="text" value={telefone} placeholder="Telefone Celular" maxLength={14} onChange={(e) =>{
+                    const inputValue = e.target.value;
+                    const numericValue = inputValue.replace(/\D/g, '');
+                    setTelefone(numericValue);
+                    let newValue = inputValue;
+                    if (inputValue.length == 11 ) {
+                    newValue = inputValue.substring(0,0) + "(" + inputValue.substring(0,2) + ")" + inputValue.substring(2,7) + "-" + inputValue.substring(7) ;
+                    setTelefone(newValue)
+    }
+                }}/>
+                <input type="text" placeholder="Cep" value={cep} maxLength={9} onChange={(e) =>{
+                    const inputValue = e.target.value;
+                    const numericValue = inputValue.replace(/\D/g, '');
+                    setCep(numericValue);
+                    let newValue = inputValue;
+                    if (inputValue.length == 8 && e.nativeEvent.inputType !== "deleteContentBackward") {
+                    newValue = inputValue.substring(0,5) + "-" + inputValue.substring(5);
+                    setCep(newValue)
+    }
+                }}/>
+                
+                <input type="text" placeholder="Senha" value={senha} minLength={8} maxLength={16}  onChange={(e) =>{
+                    setSenha(e.target.value);
+                }} onBlur={() => {
+                    const inputValue = document.getElementById("sa");
+                    if(senha.length < 8)
+                        inputValue?.classList.toggle("ats");
+                    
+                }} onFocus={()=>{
+                    const inputValue = document.getElementById("sd");
+                    if(senha.length < 8)
+                    inputValue?.classList.toggle("ats");
+                }}/>
+                <label id="sda"> kkk</label>
+                <input type="text" placeholder="Repita sua Senha"  value={senhaRepetir} minLength={8} maxLength={16} onChange={(e) =>{
+                    setSenhaRepetir(e.target.value);
+                }} onBlur={() => {
+                    const inputValue = document.getElementById("sda");
+                    if(senhaRepetir.length < 8 || senhaRepetir !== senha )
+                        inputValue?.classList.toggle("ats");
+                    
+                }} onFocus={()=>{
+                    const inputValue = document.getElementById("sda");
+                    if(senhaRepetir.length < 8 || senhaRepetir !== senha)
+                    inputValue?.classList.toggle("ats");
+                }}/>
                 <input type="checkbox"/>
                 <label>Li e aceito as <a href="#">Condições Legais</a> e a <a href="#">Política de Privacidade</a></label>
                 <input type="checkbox"/>
@@ -22,10 +69,8 @@ export default function CadastroPessoa(){
                 <p>Você é uma empresa? <a href="#">Cadastre-se aqui</a></p>
             </div>
             <div className="redes">
-
             </div>
             <div className="imagem">
-
             </div>
             </div>
             
