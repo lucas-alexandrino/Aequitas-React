a8f718a57f23ba818c4cca96f545a50f93645c13
Brunodw123br <brunosilvandradeDev@gmail.com>
2024-06-07 07:44:56 -0300

    telas já existentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import './App.css'
+import HeaderEmpressa from './componentes/HeaderEmpressa/HeaderEmpressa'
+import CadastroEmpresa from './pages/CadastroEmpresa/CadastroEmpresa'
 import CadastroPessoa from "./pages/CadastroPessoa/CadastroPessoa"
 import Login from './pages/Login/Login'
 
@@ -7,7 +9,7 @@ function App() {
   return (
     <>
       <div className='container'>
-        <CadastroPessoa/>
+        <CadastroEmpresa/>
       </div>
     </>
   )
