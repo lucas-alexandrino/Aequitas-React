a9a2dad2f63e60fa4fb925ba61d25ccea593e9f9
luisfbarreto <dev.luisf17th@gmail.com>
2024-06-17 22:20:51 -0300

    Updates

--- src/componentes/HeaderBanco/MidBanco.tsx
+++ src/componentes/HeaderBanco/MidBanco.tsx
@@ -2,6 +2,7 @@ import "./midBanco.css"
 import fundoOracle from "../../images/capaoracle.png"
 import logoOracle from "../../images/perfiloracle.png"
 import line from "../../images/Line.png"
+import gambi from "../../images/gambi.png"
 export default function MidBanco() {
 
     return (
@@ -15,11 +16,12 @@ export default function MidBanco() {
                     <img src={fundoOracle} alt="Fundo Oracle" />
                     </div>
 
-               <div className="perfilOracle">
-                <img src={logoOracle} alt="Perfil" />
-                <p>Oracle</p>
-                </div>
-                <img src={line} className="line" alt="Fundo Oracle" />
+               {/* <div className="perfilOracle"> */}
+                {/* <img src={logoOracle} alt="Perfil" /> */}
+                {/* <p>Oracle</p> */}
+                {/* </div> */}
+                {/* <img src={line} className="line" alt="Fundo Oracle" /> */}
+                <img className="gambi" src={gambi} alt="Gambiarra" />
             </div>
             }
         </>
