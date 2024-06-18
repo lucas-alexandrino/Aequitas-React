db88bc42ab28178075ff3e2b9137238abae035f5
luisfbarreto <dev.luisf17th@gmail.com>
2024-06-17 22:27:23 -0300

    Updates

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,11 @@ function App() {
   return (
     <>
       <div className='container'>
+<<<<<<< Updated upstream
         <BancoTalento/>
+=======
+        <CadastroPessoa/>
+>>>>>>> Stashed changes
       </div>
     </>
   )
