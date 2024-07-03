f39c90af0b93fc30f8e9f4f90e4cae7c8ca77414
Lucas Alexandrino <lucas.alexcontato@gmail.com>
2024-07-02 22:30:29 -0300

    Alterações

--- src/componentes/perfil/Perfil.tsx
+++ src/componentes/perfil/Perfil.tsx
@@ -1,5 +1,5 @@
 
-import { useState } from "react";
+
 
 import './Perfil.css';
 // Icons
--- src/componentes/vagas/Vagas.tsx
+++ src/componentes/vagas/Vagas.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import "./Vagas.css";
 import "../vagasampliada/VagasAmpliada.css";
 import { Grid } from "@mui/material";
